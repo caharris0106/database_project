bb85fc88da305eaeabffd5dd4fe4fc060c2c7902
caharris0106 <caharris0106@gmail.com>
2018-08-25 14:52:33 -0400

    updated html

--- backend.py
+++ backend.py
@@ -2,15 +2,25 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+
 @app.route("/")
-def hellothere():
-    return "Hello World!"
+def home():
+    return render_template('homepage.html')
 
-@app.route('/name')
-def name():
-    return 'Cody'
+# @app.route('/name')
+# def name():
+#     return 'Cody'
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     return render_template('hello.html', name=name)
+
+# @app.route('/home')
+# def crud():
+#     return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run()
+
+g_api='AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8'
