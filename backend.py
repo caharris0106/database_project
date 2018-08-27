4c35dac80091069cfa9eac77800405af466a5799
caharris0106 <caharris0106@gmail.com>
2018-08-27 12:41:16 -0400

    added apges and css

--- backend.py
+++ backend.py
@@ -1,24 +1,26 @@
 from flask import Flask
-from flask import render_template
-
+from flask import render_template, request
+import wtforms import Form
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('homepage.html')
 
-# @app.route('/name')
-# def name():
-#     return 'Cody'
+@app.route("/about")
+def about():
+    return render_template('about.html')
 
-# @app.route('/hello/')
-# @app.route('/hello/<name>')
-# def hello(name=None):
-#     return render_template('hello.html', name=name)
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
-# @app.route('/home')
-# def crud():
-#     return render_template('homepage.html')
+@app.route("/register")
+def register():
+    return render_template('register.html')
+
+class RegistrationForm(Form):
+
 
 if __name__ == '__main__':
     app.run()
