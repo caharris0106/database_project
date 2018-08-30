7a9cbfe998ef690e54bee964a4a682f3a001d3b5
caharris0106 <caharris0106@gmail.com>
2018-08-29 22:56:00 -0400

    added mail, booklist in account

--- backend.py
+++ backend.py
@@ -1,13 +1,25 @@
 from flask import Flask, flash, redirect, request, url_for, session, logging
 from flask import render_template, request
+from flask_mail import Mail, Message
 from wtforms import Form
 import requests
 import sqlite3
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
 import random
+import json
+g_api='AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8'
 
 app = Flask(__name__)
+mail = Mail(app)
+
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'booklistsender1000@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Reccos:0106'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
 
 # Class Used to take user input from register.html
 class RegisterForm(Form):
@@ -38,6 +50,7 @@ class LoginForm(Form):
     password = PasswordField('Password', [validators.InputRequired()])
 
 class UserDB():
+    """This Class deals with registration"""
     # Create users Table if not already there
     def __init__(self):
         conn = sqlite3.connect('users.db')
@@ -69,10 +82,68 @@ class UserDB():
         else:
             return False
 
-    def make_books_db(self):
-        conn = sqlite3.connect("user.db")
+    def grab_info(self, username):
+        conn = sqlite3.connect("users.db")
         cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS books (id INT, username TEXT, email TEXT, books Text, authors, glink Text)")
+        # Gets the user id, username, email
+        user_row = cur.execute("SELECT id,username,email FROM users where username=?", (username,)).fetchone()
+        conn.commit()
+        conn.close()
+        return user_row
+
+class BooksDB():
+    '''This Class Deals with the book Table'''
+    def __init__(self):
+        conn = sqlite3.connect("users.db")
+        cur = conn.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS books (id INT, username TEXT, email TEXT, book Text, authors TEXT, googleID TEXT)")
+        conn.commit()
+        conn.close()
+
+    def grab_info(self, username):
+        conn = sqlite3.connect("users.db")
+        cur = conn.cursor()
+        # Gets the user id, username, email
+        user_row = cur.execute("SELECT id,username,email FROM users where username=?", (username,)).fetchone()
+        conn.commit()
+        conn.close()
+        return user_row
+
+    def insert_book(self, id, username, email, book, authors, googleID):
+        conn = sqlite3.connect("users.db")
+        cur = conn.cursor()
+        book_indb = cur.execute("SELECT * from books WHERE book=? AND username=?",(book,username)).fetchone()
+        if book_indb==None:
+            # Insert into Books, id, username, email, book, authors, glink
+            cur.execute("INSERT INTO books VALUES (?,?,?,?,?,?)", (id, username, email, book, authors, googleID))
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            return False
+    def delete_book(self, book, username):
+        conn=sqlite3.connect("users.db")
+        cur = conn.cursor()
+        book_indb = cur.execute("SELECT * from books WHERE book=? AND username=?",(book,username)).fetchone()
+        if book_indb!=None:
+            # Insert into Books, id, username, email, book, authors, glink
+            cur.execute("DELETE FROM books WHERE username=? AND book=?", (username, book))
+            conn.commit()
+            conn.close()
+            return True
+        else:
+            return False
+    def create_json(self, username):
+        # Return a Json File from books db for display
+        conn=sqlite3.connect('users.db')
+        cur=conn.cursor()
+        jfile = cur.execute('SELECT book, authors, googleID FROM books WHERE username=?', (username,)).fetchall()
+        books = dict(total=0, items=list())
+        for book, authors, googleID in jfile:
+            books['items'].append(dict(book=book,authors=authors, googleID=googleID))
+            books['total'] +=1
+        return books
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -101,14 +172,13 @@ def login():
     form = LoginForm(request.form)
     if request.method == 'POST':
         # Get Form Fields:
-        # username = request.form.get('uname')
-        # password_candidate = request.form.get('password')
         username = form.username.data
         password_candidate = form.password.data
         db = UserDB()
         if db.find_user(username, password_candidate):
             session['logged_in'] = True
             session['username'] = username
+
             flash("Logged in as {}!".format(username))
             return redirect(url_for('dashboard'))
         else:
@@ -124,39 +194,104 @@ def login():
 @app.route("/")
 def home():
     return render_template('home.html', books=requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
-            random.choice('abcdefghijklmnopqrstuvwxzy') +
+            'python' +
              "&maxResults=40&key=AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8").json())
+
+@app.route("/search", methods=["GET","POST"])
+def search():
+
+    if request.method=='POST':
+        
+        if not request.form.get("title"):
+            flash("Enter a title")
+
+        if request.form.get("author"):
+            return render_template("searchResults.html", books = requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
+                               request.form.get("title") + "+inauthor:" + request.form.get("author") +
+                               "&key=AIzaSyBtprivgL2dXOf8kxsMHuELzvOAQn-2ZZM").json())
+
+        if request.form.get("author"):
+            return render_template("searchResults.html", books = requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
+                               request.form.get("title") + "+isbn:" + request.form.get("isbn") +
+                               "&key=AIzaSyBtprivgL2dXOf8kxsMHuELzvOAQn-2ZZM").json())
+
+        if request.form.get("author") + request.form.get("isbn"):
+            return render_template("searchResults.html", books = requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
+                               request.form.get("title") + "+inauthor:" + request.form.get("author") +
+                               "isbn:" + request.form.get("isbn") +
+                               "&key=AIzaSyBtprivgL2dXOf8kxsMHuELzvOAQn-2ZZM").json())
+
+        return render_template("searchResults.html", books=requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
+                    request.form.get("title") +
+                    "&key=AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8").json())
+
+    return render_template("search.html")
+
+@app.route("/searchResults", methods=["GET","POST"])
+def searchResults():
+    return render_template("searchResults.html")
 
 @app.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html', session=session)
+    books = BooksDB().create_json(session['username'])
+    # if request.method == 'POST':
+    #
+    #     msg = Message('Thank you for registration', sender = 'booklistsender1000@gmail.com', recipients = ['caharris0106@gmail.com'])
+    #
+    #     # define content of the email
+    #     msg.body = """Hello and thank you for join us! We hope you enjoy at our site where you can search, read, comment and grade your favourite books. Have fun!"""
+    #
+    #     # send email
+    #     mail.send(msg)
+    return render_template('dashboard.html', session=session, books=books)
 
 @app.route("/about")
 def about():
     return render_template('about.html')
 
+@app.route("/redir/<title>/<author>/<googleID>")
+def redir(title, author, googleID):
+    book_db = BooksDB()
+    user_id = book_db.grab_info(session['username'])[0]
+    email = book_db.grab_info(session['username'])[2]
+    book_title = title
+    book_id = googleID
+    book_author = author.replace('[','').replace(']','').replace('\'','')
+    book_db.insert_book(user_id, session['username'], email, book_title, book_author, book_id)
+    return redirect(url_for('dashboard'))
+
 @app.route("/logout")
 def logout():
     session.clear()
+    # random.choice('abcdefghijklmnopqrstuvwxzy')
     return render_template('home.html', books=requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
-            random.choice('abcdefghijklmnopqrstuvwxzy') +
+            'computer' +
              "&maxResults=40&key=AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8").json())
 
 if __name__ == '__main__':
+
     app.secret_key = 'secret123'
     app.run(debug=True)
-    books = requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
-            random.choice('abcdefghijklmnopqrstuvwxzy') +
-             "&maxResults=40&key=AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8").json()
-    # print(type(books.json()['items']))
-    for x in books:
-        print(x)
-    # username='doorman'
-    # conn = sqlite3.connect("users.db")
+    # conn = sqlite3.connect('users.db')
     # cur = conn.cursor()
-    # check_existing = cur.execute("SELECT * FROM users").fetchall()
-    #
-    # print(check_existing)
-    # x = sha256_crypt.verify('llabtoof', rows)
-
-# g_api='AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8'
+    # cur.execute("DROP TABLE books")
+    # conn.commit()
+    # conn.close()
+    # book= 'Principles'
+    # book_indb = cur.execute("SELECT * from books WHERE book=?",(book,)).fetchone()
+    # if book_indb==None:
+    #     # Insert into Books, id, username, email, book, authors, glink
+    #     cur.execute("INSERT INTO books VALUES (?,?,?,?,?)", (2, 'a','a',book,'a'))
+    #     conn.commit()
+    #     conn.close()
+    # username='charris52'
+    # conn=sqlite3.connect('users.db')
+    # cur=conn.cursor()
+    # jfile = cur.execute('SELECT book, authors, googleID FROM books WHERE username=?', (username,)).fetchall()
+    # file_test = BooksDB().create_json(username)
+    # print(file_test)
+    # bks=requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
+    #         'a' +
+    #         "&key=AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8").json()
+    # print(type(bks))
+    # g_api='AIzaSyAvHykLgaS8U3WrOp48sbNcI_lAtBmLyD8'
